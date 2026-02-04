1db7a51df4fa0fe56db70f5dab36f4a55598579d
AADI <adi@Aadi-MacBook-Air.local>
2026-02-04 22:50:44 +0530

    Force backend URL to verify production

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://my-project-backend-gnqn.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
